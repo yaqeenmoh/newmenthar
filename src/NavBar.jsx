10d9ec955d6f20befdfcbb72c3cd33f8329df657
yaqeen <yaqeen_mohammed@yahoo.com>
2019-04-07 13:43:49 -0700

    col

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -24,55 +24,12 @@ class NavBar extends Component {
 
     render() {
       return (
-<<<<<<< HEAD
-      
-      
-      <nav className="navbar navbar-dark navbar-expand-md primary-color"
-      role="navigation">
-      
-      <div className="header-desktop-leftside col-md-4">
-      
-      <ul className="header-desktop-profileicons nav col-md-6">
-      
-      
-      <li className="nav-item"><a
-      href="#!"><img
-      className="margin"
-      src="/static/media/question-mark.ec349e0f.svg"></img>
-      
-      </a></li>
-      
-      <li className="nav-item"><a
-      href="#!">
-      <img className="margin"
-      src={questionmark}></img></a></li>
-      
-      <li className="nav-item"><a
-      href="#!"><img
-      src={notification1}alt=""
-      className="margin"></img>
-      
-      </a></li>
-      
-      </ul>
-      
-      <div className="header-desktop-profile col-md-6">
-      
-      <img src="/static/media/girl.6b23eaa1.png"
-      alt=""style=
-      {{opacity:0.75,width:38.15,
-      height:35.02}}
-      className="rounded-circle header-desktop-profilepic "
-      />
-      
-      <div className="dropdown"><a
-=======
         <header>
           <div className="topnav">
           <nav className="navbar navbar-dark navbar-expand-md primary-color"
       role="navigation">
        <div className="header-desktop-leftside col-md-4">
-       <ul className="header-desktop-profileicons nav col-md-6">
+       <ul className="header-desktop-profileicons nav">
      
      
      <li className="nav-item"><a
@@ -86,7 +43,8 @@ class NavBar extends Component {
      <li className="nav-item"><a
      href="#!">
      <img className="marginright"
-     src={chat1}></img></a></li>
+     src={chat1}></img></a>
+     </li>
     
      <li className="nav-item"><a
      href="#!"><img
@@ -97,7 +55,6 @@ class NavBar extends Component {
      </li>
      <li className="nav-item">
      <div ><a
->>>>>>> 193522388f2f0824215037f294365728035d8fbb
       aria-haspopup="true"
       href="#" 
       className="dropdown-toggle nav-link white-text"
@@ -127,16 +84,6 @@ class NavBar extends Component {
       </div>
       
       </div>
-<<<<<<< HEAD
-      
-      </div>
-      
-      </div>
-      
-      <div className="col-md-6">
-      
-      <input placeholder="ابحث عن معلمين, دورات ,أوراق عمل"
-=======
       </li>
      </ul>
     
@@ -147,14 +94,9 @@ class NavBar extends Component {
     
      
       <input placeholder="ابحث  عن  دورات , معملين , أوراق عمل"
->>>>>>> 193522388f2f0824215037f294365728035d8fbb
       type="text"
       className="form-control custom-control-text2 header-input customInput"
       style={{color: "white", opacity: 1}}/>
-<<<<<<< HEAD
-      
-      </div><ul className="navbar-nav ml-auto col-md-2"><div
-=======
      
       
      
@@ -162,24 +104,13 @@ class NavBar extends Component {
      
      <div className="right-nav-bar">
      <ul className="navbar-nav ml-auto col-md-2"><div
->>>>>>> 193522388f2f0824215037f294365728035d8fbb
       className="navbar-brand"><div
       className="container">
       
       <div className="row"><img
-<<<<<<< HEAD
-      src={path}
-      alt="" 
-      className="rounded-circle" 
-      style= {{width:50,
-      height:50}}/>
-      
-      
-=======
       src={path}/>
       
      
->>>>>>> 193522388f2f0824215037f294365728035d8fbb
       </div>
       
       </div>
@@ -189,26 +120,6 @@ class NavBar extends Component {
       
       
       </ul>
-<<<<<<< HEAD
-      
-      
-      
-      
-      </nav>
-      
-      
-      
-      
-      
-      );
-      
-      }
-      
-      
-
-
-}
-=======
      </div>
     
      
@@ -222,6 +133,5 @@ class NavBar extends Component {
       );
     }
   }
->>>>>>> 193522388f2f0824215037f294365728035d8fbb
 
 export default NavBar;
